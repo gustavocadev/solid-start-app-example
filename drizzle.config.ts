4e405a321102722218634ac7522f3835b97763d6
gustavocadev <gustavocadev@gmail.com>
2024-07-23 14:32:08 -0500

    Update drizzle.config.ts to load environment variables and use SQLite dialect

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,12 @@
-import 'dotenv/config';
-import type { Config } from 'drizzle-kit';
+import type { Config } from "drizzle-kit";
+
+process.loadEnvFile();
 
 export default {
-  schema: './src/lib/drizzle/schema.ts',
-  out: './drizzle',
-  driver: 'better-sqlite', // 'pg' | 'mysql2' | 'better-sqlite' | 'libsql' | 'turso'
+  schema: "./src/lib/drizzle/schema.ts",
+  out: "./drizzle",
+  dialect: "sqlite",
   dbCredentials: {
-    url: process.env.DATABLE_URL!,
+    url: process.env.DATABLE_URL ?? "",
   },
 } satisfies Config;
