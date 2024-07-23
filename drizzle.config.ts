f7d01d40b9bdf5da227e45e6c3fa81d7c81f5872
gustavocadev <gustavocadev@gmail.com>
2024-07-23 14:36:06 -0500

    Update drizzle.config.ts to use consistent quotes and load environment variables

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
-import type { Config } from "drizzle-kit";
+import type { Config } from 'drizzle-kit';
 
 process.loadEnvFile();
 
 export default {
-  schema: "./src/lib/drizzle/schema.ts",
-  out: "./drizzle",
-  dialect: "sqlite",
+  schema: './src/lib/drizzle/schema.ts',
+  out: './drizzle',
+  dialect: 'sqlite',
   dbCredentials: {
-    url: process.env.DATABLE_URL ?? "",
+    url: process.env.DATABLE_URL ?? '',
   },
 } satisfies Config;
